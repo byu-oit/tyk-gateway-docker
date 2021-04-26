dcc207951460471a16cc1e4a49f469479db1d4f2
David Caldwell <dwclogic@gmail.com>
2021-04-26 14:23:20 -0600

    Working on the addPeopleSoftWSSecurityHeader.js file, and ran into Header key capitolization issues.

--- middleware/addPeopleSoftWSSecurityHeader.js
+++ middleware/addPeopleSoftWSSecurityHeader.js
@@ -1,48 +1,109 @@
 var addPeopleSoftWSSecurityHeader = new TykJS.TykMiddleware.NewMiddleware({});
 
 addPeopleSoftWSSecurityHeader.NewProcessRequest(function(request, session, config) {
-    log("|- addPeopleSoftWSSecurityHeader() ---|");
-    //log("|--- request.ReturnOverrides: " + JSON.stringify(request.ReturnOverrides) );
-    log("|--- session: " + JSON.stringify(session) );
-    var headerResults = checkHeaders(request.Headers);
-    if(headerResults){
-        log("|--- headerResults NOT undefined : " + JSON.stringify(headerResults) );
-        request.ReturnOverrides = headerResults;
-        log("|--- headerResults NOT undefined : " + JSON.stringify(headerResults) );
+    log("|- addPeopleSoftWSSecurityHeader() START---|");
+    log("|- " + JSON.stringify(request));
+    ps_WSS_RemoveHeaders(request);
+    log("|--- request.DeleteHeaders: " + JSON.stringify(request.DeleteHeaders) );
+
+    var errorResults = undefined  // on error return object of error data for returnOverrides.
+    //
+    if (request.Headers["Acting-For"] == "false"){
+        if (request.Headers["Resourceowner_net_id"]) {
+            request.SetHeaders["Effective_net_id"] = request.Headers["Resourceowner_net_id"];
+            log("1- Successful ActingFor == false: effective_net_id set to: " + request.Headers["Resourceowner_net_id"]);
+        }
+        else {
+            log("|---1 ActingFor NOT false : ");
+            errorResults = {
+                ResponseError: "Invalid Request",
+                ResponseBody: "Header Missing from request",
+                ResponseCode: 400,
+                ResponseHeaders: {}
+            }
+            log("|---1 ActingFor NOT false : ");
+        }
     }
     else {
-        //timestamp_middleware = new Date().getTime();
-        //request.SetHeaders['byu-entity'] = create_byuEntity();
-        var secret = "secret";
-        var message = create_byuEntity();
-        var hash = CryptoJS.HmacSHA256( message, secret );
-        var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
-        request.SetHeaders['auth-jwt-byu-entity'] = hashInBase64;
-    }
-    log(JSON.stringify(request.Headers));
-    return addPeopleSoftWSSecurityHeader.ReturnData(request, {});
-});
-
-/* check headers
- */
-function checkHeaders(headers){
-    log("|- checkHeaders(headers) ---|");
-    log("|--- " + JSON.stringify(headers));
-    var item;
-    for(item in headers){
-        log("|----- " + item);
-        if(item.toLowerCase() == "smuser"){
-            return {
-                ResponseError: "Invalid Request",
-                ResponseBody: "Invalid Request",
-                ResponseCode: 400,
-                ResponseHeaders : {
-                    "X-Foo": "Bar",
-                    "X-Baz": "Qux"
+       var actingFor = canUseActorPermissions()  // db call: probably replaced with Redis call
+        if (actingFor == false){
+            log("2|--- DB Query canUserActorPermissions == false : " );
+            errorResults = {
+                ResponseError: "Not Authorized",
+                ResponseBody: "Not Authorized",
+                ResponseCode: 403,
+                ResponseHeaders: {}
+            }
+            log("2|--- DB Query canUserActorPermissions == false : ");
+        }
+        else {
+            if  (request.Headers["Actor_net_id"]){
+                request.SetHeaders["effective_net_id"] = request.Headers["Actor_net_id"];
+                log("2- Successful ActingFor permissions: effective_net_id: " + request.Headers["Actor_net_id"]);
+            }
+            else {
+                log("|---2 Missing Property : ");
+                errorResults = {
+                    ResponseError: "Invalid Request",
+                    ResponseBody: "Header Missing from request",
+                    ResponseCode: 400,
+                    ResponseHeaders: {}
                 }
+                log("|---2 Missing Property : ");
             }
         }
     }
-    return undefined;
+
+    if (errorResults != undefined){
+        request.ReturnOverrides = errorResults;
+        log("ReturnOverrides" + JSON.stringify(request.ReturnOverrides));
+    }
+
+    return addPeopleSoftWSSecurityHeader.ReturnData(request, {});
+});
+
+function canUseActorPermissions(){
+    // there is a function that calls a Database, we're thinking redis cache of info
+    // Returning true or false depending on actors cached identity permissions for such.
+    // use method: TykGetKeyData(api_key, api_id) to get cached data (BLOCKING CALL)
+    // 50/50 true false return.
+    var timestamp = new Date().getTime();
+    var actingFor = (timestamp%2?true:false);
+    log("|-- canUseActorPermissions result = " + actingFor);
+    return true;
 }
 
+/* check headers
+ */
+function ps_WSS_RemoveHeaders(request){
+    log("|- checkHeaders(headers) ---|");
+    var x = 0
+    for(var item in request.Headers) {
+        item = item.toLowerCase();
+        switch (item) {
+            case "sm_user":
+                request.DeleteHeaders[x++] = item;
+                break;
+            case "principal_net_id":
+                request.DeleteHeaders[x++] = item;
+                break;
+            case "principal_byu_id":
+                request.DeleteHeaders[x++] = item;
+                break;
+            case "principal_id":
+                request.DeleteHeaders[x++] = item;
+                break;
+            case "actor_id":
+                request.DeleteHeaders[x++] = item;
+                break;
+            case "actor_byu_id":
+                request.DeleteHeaders[x++] = item;
+                break;
+            case "actor_net_id":
+                request.DeleteHeaders[x++] = item;
+                break;
+            default:
+                break;
+        }
+    }
+}
--- middleware/rejectSmUserHeader.js
+++ middleware/rejectSmUserHeader.js
@@ -4,12 +4,12 @@ rejectSmUserHeader.NewProcessRequest(function(request, session, config) {
     log("|- NewProcessRequest() ---|");
     //log("|--- request.ReturnOverrides: " + JSON.stringify(request.ReturnOverrides) );
     log("|--- session: " + JSON.stringify(session) );
-    //var headerResults = checkHeaders(request.Headers);
-    //if(headerResults != undefined){
+    var headerResults = checkHeaders(request.Headers);
+    if(headerResults != undefined){
         log("|--- headerResults NOT undefined : " + JSON.stringify(headerResults) );
         request.ReturnOverrides = headerResults;
         log("|--- headerResults NOT undefined : " + JSON.stringify(headerResults) );
-    //}
+    }
 
     log(JSON.stringify(request.Headers));
     return rejectSmUserHeader.ReturnData(request, {});
@@ -17,23 +17,23 @@ rejectSmUserHeader.NewProcessRequest(function(request, session, config) {
 
 /* check headers
  */
-// function checkHeaders(headers){
-//     log("|- checkHeaders(headers) ---|");
-//     log("|--- " + JSON.stringify(headers));
-//     var item;
-//     for(item in headers){
-//         log("|----- " + item);
-//         if(item.toLowerCase() == "smuser"){
-//             return {
-//                 ResponseError: "Invalid Request",
-//                 ResponseBody: "Invalid Request",
-//                 ResponseCode: 400,
-//                 ResponseHeaders : {
-//                     "X-Foo": "Bar",
-//                     "X-Baz": "Qux"
-//                 }
-//             }
-//         }
-//     }
-//     return undefined;
-// }
+function checkHeaders(headers){
+    log("|- checkHeaders(headers) ---|");
+    log("|--- " + JSON.stringify(headers));
+    var item;
+    for(item in headers){
+        log("|----- " + item);
+        if(item.toLowerCase() == "sm_user"){
+            return {
+                ResponseError: "Invalid Request",
+                ResponseBody: "Invalid Request",
+                ResponseCode: 400,
+                ResponseHeaders : {
+                    "X-Foo": "Bar",
+                    "X-Baz": "Qux"
+                }
+            }
+        }
+    }
+    return undefined;
+}
