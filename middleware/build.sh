3733930b05b5e14993bb7886f91b0c6366b9f0d7
David Caldwell <dwclogic@gmail.com>
2021-05-27 17:27:41 -0600

    Plugin work for Token

--- middleware/build.sh
+++ middleware/build.sh
@@ -5,7 +5,7 @@ rm -rf /opt/tyk-gateway/middleware/bundles
 for bundle in token
 do
   rm -f ${bundle}.zip
-  /opt/tyk-gateway/tyk bundle build -m ${bundle}.json -y -k /privkey.pem -o ${bundle}.zip
+  /opt/tyk-gateway/tyk bundle build -m ${bundle}.json -y -o ${bundle}.zip
   if [[ ! -f ${bundle}.zip ]]
   then
     echo "[WARN]${bundle}.zip not built!"
