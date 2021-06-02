168d165bc0701a9d35b366e906da094e73323261
David Caldwell <dwclogic@gmail.com>
2021-06-02 16:22:04 -0600

    Plugin work for Token and the Response Hook fixing hook error it's killing the call now

--- middleware/token.py
+++ middleware/token.py
@@ -1,13 +1,13 @@
 
 from gateway import TykGateway as tyk
 from tyk.decorators import *
-#import requests, json
+import requests, json
 
 @Hook
 def TokenResponseMiddleware(request, response, session, metadata, spec):
     loglevel = "info"
     auth_header = request.get_header('Authorization')
-    tyk.log("TokenResponseMiddleware |---", loglevel)
+    tyk.log("TokenResponseMiddleware Begin |---", loglevel)
 #     if auth_header == 'Bearer 19cdb7bd93e8849f1b3260ce7741c0e13':
 #         tyk.log("if auth_header == true: START", loglevel)
 #         session.rate = 1000.0
@@ -15,7 +15,8 @@ def TokenResponseMiddleware(request, response, session, metadata, spec):
 #         metadata["token"] = "47a0c79c427728b3df4af62b9228c8ae"
 #         request.add_header("Client-Id","dwclogic")
 #         request.headers["Client-Id"] = "bl645"
-    responseText = getTokenIntrospection()
+#     responseText = getTokenIntrospection()
+    tyk.log("TokenResponseMiddleware END |---", loglevel)
     return request, response, session, metadata, spec
 
 def getTokenIntrospection():
--- samples/curl-samples.sh
+++ samples/curl-samples.sh
@@ -443,13 +443,44 @@ curl -X POST -H "x-tyk-authorization: db471355-0424-423a-8f10-0d8f7618c479" \
   #result
 {
     "action": "added",
-    "key": "19cdb7bd93e8849f1b3260ce7741c0e13",
-    "key_hash": "4a90e05d",
+    "key": "16ba7956176a24837af5f87a8b8e12aba",
+    "key_hash": "b7b260b6",
     "status": "ok"
 }
+
 
 # ---- Reboot loses keys
 
 
 curl -X -H "x-tyk-authorization: db471355-0424-423a-8f10-0d8f7618c479" http://localhost:8080/tyk/keys | python -mjson.tool
 
+curl -X POST -H "x-tyk-authorization: db471355-0424-423a-8f10-0d8f7618c479" \
+  -s \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d '{
+    "allowance": 1000,
+    "rate": 1000,
+    "per": 1,
+    "expires": -1,
+    "quota_max": -1,
+    "org_id": "1",
+    "quota_renews": 1449051461,
+    "quota_remaining": -1,
+    "quota_renewal_rate": 60,
+    "access_rights": {
+      "1001": {
+        "api_id": "1001",
+        "api_name": "Hydra Token Request",
+        "versions": ["Default"]
+      }
+    },
+    "meta_data": {}
+  }' http://localhost:8080/tyk/keys/create | python -mjson.tool
+# --------
+{
+    "action": "added",
+    "key": "1be36639238c141ad819fab72fed109ff",
+    "key_hash": "1ab3e40a",
+    "status": "ok"
+}
