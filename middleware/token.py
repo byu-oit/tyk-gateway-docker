2264157448aec4bd7b72ce6e269c4252fd18f2d6
David Caldwell <dwclogic@gmail.com>
2021-06-08 14:23:29 -0600

    token.zip finished it's performing token introspection and createing a jwt.  Also moved javascript files to the ./middleware/javascript/....

--- middleware/token.py
+++ middleware/token.py
@@ -1,23 +1,13 @@
 
 from gateway import TykGateway as tyk
 from tyk.decorators import *
-
-#import os
-#import sys
-# bundle_dir = os.path.abspath(os.path.dirname(__file__))
-# for lib_dir in [ 'vendor/lib/python3.6/site-packages/', 'vendor/lib64/python3.6/site-packages/' ]:
-#   vendor_dir = os.path.join(bundle_dir, lib_dir)
-#   sys.path.append(vendor_dir)
-
-import json
-import requests
+import json, requests, base64
+import jwt
 
 @Hook
 def TokenResponseMiddleware(request, response, session, metadata, spec):
     logLevel = "info"
     tyk.log("TokenResponseMiddleware Begin |---", logLevel)
-    tyk.log("bundle_dir" + os.path.abspath(os.path.dirname(__file__)), logLevel)
-#     tyk.log("|--- ResponseHook response object: " + str(response), logLevel)
 
     bodyJsonStr = response.raw_body.decode()
 #     tyk.log("|--- bodyJsonStr=" + str(bodyJsonStr), logLevel)
@@ -28,14 +18,13 @@ def TokenResponseMiddleware(request, response, session, metadata, spec):
     tyk.log("|--- access_token = " + str(access_token), logLevel)
     expires_in = body["expires_in"]
     tyk.log("|--- expires_in = " +  str(expires_in), logLevel)
-    token_type = body["token_type"]
-    tyk.log("|--- token_type = " +  str(token_type), logLevel)
-    scope = body["scope"]
-    tyk.log("|--- scope = " +  str(scope), logLevel)
+#     token_type = body["token_type"]
+#     tyk.log("|--- token_type = " +  str(token_type), logLevel)
+#     scope = body["scope"]
+#     tyk.log("|--- scope = " +  str(scope), logLevel)
 
 #   Token Introspection ----------------------------------------------------------------------
     token_url = "https://oauth.api-dev.byu.edu/oauth2/introspect"
-
     data = {'token': access_token}
     headersData = {
         'Authorization':'Bearer ' + access_token,
@@ -43,22 +32,26 @@ def TokenResponseMiddleware(request, response, session, metadata, spec):
         'Accept':  '*/*'
     }
     access_token_response = requests.post(token_url, data=data, headers=headersData)
-    tyk.log(str(access_token_response.headers), logLevel)
-    tyk.log(access_token_response.text, logLevel)
+#     tyk.log(str(access_token_response.headers), logLevel)
+#     tyk.log(access_token_response.text, logLevel)
+    iData = json.loads(access_token_response.text)
+    tyk.log(str(iData), logLevel)
 
-    introspectionData = json.loads(access_token_response.text)
-    tyk.log(str(introspectionData), logLevel)
+#   Generate JWT HERE or on first use of Token -----------------------------------------------
 
-#     api_call_headers = {'Authorization': 'Bearer ' + tokens['access_token']}
-#     api_call_response = requests.get(test_api_url, headers=api_call_headers, verify=False)
+    with open('/opt/tyk-gateway/middleware/jwtRS256.key', 'rb') as fh:
+        signing_key = jwt.jwk_from_pem(fh.read())
 
-#   Generate JWT HERE or on first use of Token?
+    new_jwt = jwt.JWT().encode(iData, signing_key, alg='RS256')
+#     tyk.log(str("Signature: " + new_jwt), logLevel)
 
-    tyk.store_data(access_token, "access_token is the key to this sentence.", expires_in)
+#     new_jwt = str(jotHdr64) + "." + str(mStr64) + "." + signature
+    tyk.log("new_jwt: " + str(new_jwt), logLevel)
+#   Store JWT under the access_token ---------------------------------------------------------
+    tyk.store_data(access_token, new_jwt, expires_in)
 
-    token_data = tyk.get_data(access_token)
-    tyk.log("access_token data stored = " + str(token_data), logLevel)
+#     token_data = tyk.get_data(access_token)
+#     tyk.log("access_token data stored = " + str(token_data), logLevel)
 
     tyk.log("TokenResponseMiddleware END |---", logLevel)
     return request, response, session, metadata, spec
-
