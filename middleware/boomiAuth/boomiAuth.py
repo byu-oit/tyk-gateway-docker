9bf25fe6b7b50a8c097c1fd51994557c52bd83f2
leebrucec <leebrucec@gmail.com>
2021-06-21 09:53:31 -0600

    Changes to sync up with dev branch

--- middleware/boomiAuth/boomiAuth.py
+++ middleware/boomiAuth/boomiAuth.py
@@ -1,52 +1,26 @@
 from tyk.decorators import *
 from gateway import TykGateway as tyk
-# # import json, requests
-
-# import os
-# import sys
-
-# bundle_dir = os.path.abspath(os.path.dirname(__file__))
-# for lib_dir in [ 'vendor/lib/python3.7/site-packages/' ]:
-#   vendor_dir = os.path.join(bundle_dir, lib_dir)
-#   sys.path.append(vendor_dir)
-
-# sys.path.append(bundle_dir)
-
-# import boomiAuthUtil
-
-
-# @Hook
-# def MyPreMiddleware(request, session, metadata, spec):
-#     loglevel = "info"
-#     tyk.log("MyPreMiddleware START", loglevel)
-
-#     # MyUtilFunction(request, session, metadata, spec)
-
-#     auth_header = request.get_header('Authorization')
-#     if auth_header:
-#         tyk.log('Authorization = ' + auth_header, loglevel)
-
-#     tyk.log("MyPreMiddleware END", loglevel)
-#     return request, session, metadata
 
 @Hook
 def BoomiAuthMiddleware(request, session, spec):
-    # boomiAuthUtil.log()
     tyk.log("*** BoomiAuthMiddleware START", "indo")
-    url = request.object.url
-    tyk.log("*** url = " + url, "info")
-    body = request.object.body
-    tyk.log("*** body = " + body, "info")
+
+    # LogHeaders(request.object.headers)
 
     # check for x-Jwt-Assertion header
     x_jwt_assertion = request.get_header('X-Jwt-Assertion')
 
     # if we have the header, insert header X-Wss-Jwt-Assertion with this value
     if x_jwt_assertion:
+        tyk.log("*** Found header X-Jwt-Assertion", "info")
+        # add new heaer
         request.add_header("X-Wss-Jwt-Assertion", x_jwt_assertion)
-
-    # delete X-Jwt-Assertion header
-    request.delete_header('X-Jwt-Assertion')
+        # delete X-Jwt-Assertion header
+        request.delete_header('X-Jwt-Assertion')
 
     tyk.log("*** BoomiAuthMiddleware End", "info")
     return request, session
+
+def LogHeaders(headers):
+    for key, value in headers.items():
+        tyk.log("*** header " + key + "->" + value, "info")
--- middleware/serviceNowAuth/serviceNowAuth.py
+++ middleware/serviceNowAuth/serviceNowAuth.py
@@ -3,12 +3,13 @@ from gateway import TykGateway as tyk
 
 @Hook
 def ServiceNowAuth(request, session, spec):
-    # boomiAuthUtil.log()
     tyk.log("*** ServiceNowAuth START", "indo")
-    url = request.object.url
-    tyk.log("*** url = " + url, "info")
-    body = request.object.body
-    tyk.log("*** body = " + body, "info")
+    # url = request.object.url
+    # tyk.log("*** url = " + url, "info")
+    # body = request.object.body
+    # tyk.log("*** body = " + body, "info")
+
+    # LogHeaders(request.object.headers)
 
     # check for X-Jwt-Assertion header
     x_jwt_assertion = request.get_header('X-Jwt-Assertion')
@@ -17,31 +18,18 @@ def ServiceNowAuth(request, session, spec):
     if x_jwt_assertion:
         # concatenate XJWTAssertion with header value
         authStr = "X-Jwt-Assertion " + x_jwt_assertion
+
         # add X-Wss-Jwt-Assertion header
-        request.add_header("Authorization", authStr)
         tyk.log("add header Authorization: " + authStr, "info")
+        request.add_header("Authorization", authStr)
+
         # delete X-Jwt-Assertion header
-        request.delete_header('X-Jwt-Assertion')
         tyk.log("remove header X-Jwt-Assertion", "info")
+        request.delete_header('X-Jwt-Assertion')
 
     tyk.log("*** ServiceNowAuth End", "info")
     return request, session
 
-    # // iterate through headers looking for X-Jwt-Assertion
-    # for(var item in request.Headers) {
-    #     console.log(item + ": " + request.Headers[item]);
-
-    #     // compare formated header key
-    #     if ('X-Jwt-Assertion' == formatHeaderKey(item)) {
-    #         // concatenate XJWTAssertion with header value
-    #         var authStr = 'XJWTAssertion ' + String(request.Headers[item]);
-
-    #         // add X-Wss-Jwt-Assertion header
-    #         request.SetHeaders['Authorization'] = authStr;
-    #         console.log('\tAdd header Authorization: ' + authStr);
-
-    #         // remove X-Jwt-Assertion header
-    #         request.DeleteHeaders[0] = item;
-    #         console.log("\tRemove header: " + item);
-    #         break;
-    #     }
+def LogHeaders(headers):
+    for key, value in headers.items():
+        tyk.log("*** header " + key + "->" + value, "info")
