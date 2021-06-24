38cee629289a6f5510b1d1d6703a8b780ee8b514
David Caldwell <dwclogic@gmail.com>
2021-06-24 15:12:23 -0600

    .davebuild.sh 4 Bruce.

--- middleware/davebuild.sh
+++ middleware/davebuild.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 dir="$(pwd)"
+docker run --rm -w "/tmp" -v $(pwd):/tmp --entrypoint "/bin/sh" -it tykio/tyk-gateway:v3.1.2 -c "pip3 install -r requirements.txt -t ./vendor/lib/python3.7/site-packages/"
 for bundle in byu-token api-custom-auth
 do
   /bin/rm -f ${bundle}.zip
-  docker run --rm -w "/tmp" -v $(pwd):/tmp --entrypoint "/bin/sh" -it tykio/tyk-gateway:v3.1.2 -c "pip3 install -r requirements.txt -t ./vendor/lib/python3.7/site-packages/"
-  docker run --rm -w "/tmp" -v $(pwd):/tmp --entrypoint "/bin/sh" -it tykio/tyk-gateway:v3.1.2 -c "/opt/tyk-gateway/tyk bundle build -y -m ${bundle}.json -o ${bundle}.zip"
+    docker run --rm -w "/tmp" -v $(pwd):/tmp --entrypoint "/bin/sh" -it tykio/tyk-gateway:v3.1.2 -c "/opt/tyk-gateway/tyk bundle build -y -m ${bundle}.json -o ${bundle}.zip"
   if [[ ! -f ${bundle}.zip ]]
   then
     echo "[WARN]${bundle}.zip not built!"
